3203312d3b4b2085a5b4cb9c636dba4a7a763d47
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2017-12-12 14:48:45 +0100

    resizeObject script created

---
+++ Assets/Scripts/resizeObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resizeObject : MonoBehaviour {
+
+    public int Xsize;
+    public int Ysize;
+    public int Zsize;
+
+    bool resize;
+
+    void Start()
+    {
+        resize = true;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "interactArea" && Input.GetMouseButton(0) && resize)
+        {
+                transform.localScale += new Vector3(Xsize * Time.deltaTime, Ysize * Time.deltaTime, Zsize * Time.deltaTime);
+                if(transform.localScale.x > 8) {
+                    resize = false;
+            }
+        }
+        else if (other.tag == "interactArea" && Input.GetMouseButton(0) && !resize)
+        {
+            transform.localScale += new Vector3(-Xsize * Time.deltaTime, -Ysize * Time.deltaTime, -Zsize * Time.deltaTime);
+            if (transform.localScale.x < 3)
+            {
+                resize = true;
+            }
+        }
+
+
+    }
+}
