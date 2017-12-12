8a462b38c51157f2da18b66d179421ec1a258961
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2017-12-12 02:41:01 +0100

    rotation power has been created

---
+++ Assets/Scripts/rotateOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateOnClick : MonoBehaviour {
+
+    public int Xrotation;
+    public int Yrotation;
+    public int Zrotation;
+	
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "interactArea" && Input.GetMouseButton(0))
+        {
+            Debug.Log("a");
+            transform.Rotate(Xrotation * Time.deltaTime, Yrotation * Time.deltaTime, Zrotation * Time.deltaTime);
+        }
+    }
+}
