f2a18069bbf9edcee08f1e653b596bdf012392f4
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2017-12-12 21:56:43 +0100

    rotating puzzle programmed and prototyped

---
+++ Assets/Scripts/rotateColumns.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateColumns : MonoBehaviour
+{
+
+    public int Xrotation;
+    public int Yrotation;
+    public int Zrotation;
+
+    public GameObject[] columnToRotate;
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.tag == "interactArea" && Input.GetMouseButton(0))
+        {
+            transform.Rotate(Xrotation * Time.deltaTime, Yrotation * Time.deltaTime, Zrotation * Time.deltaTime);
+            for(int i = 0; i <= columnToRotate.Length; i++) { 
+            columnToRotate[i].transform.Rotate(Xrotation * Time.deltaTime, -Yrotation * Time.deltaTime, Zrotation * Time.deltaTime);
+            }
+
+        }
+    }
+}
+
