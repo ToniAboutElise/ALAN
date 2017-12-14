2d149046509f7041671675c74c765178155e23d3
DOMINGUEZ_REIG <antoni.dominguez@balmes.escolapia.net>
2017-12-14 17:35:57 +0100

    new puzzle has been started. Rotate objects to guide the ball to its destination. Add rigidbody to those objects too.

---
+++ Assets/Scripts/activateObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateObject : MonoBehaviour {
+    public GameObject objToActivate;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "interactArea" && Input.GetMouseButton(0))
+        {
+            objToActivate.SetActive(true);
+        }
+    }
+}
