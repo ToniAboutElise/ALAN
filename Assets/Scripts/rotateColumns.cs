5fda3fb67d9becbfe9bd2b4d7741481e5436e0cd
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2017-12-21 18:36:41 +0100

    4th puzzle programming and implementation has been started!

--- Assets/Scripts/rotateColumns.cs
+++ Assets/Scripts/rotateColumns.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class rotateColumns : MonoBehaviour
 {
-
     public int Xrotation;
     public int Yrotation;
     public int Zrotation;
 
     public GameObject[] columnToRotate;
+    public GameObject columnToRotateInvert;
 
     void OnTriggerStay(Collider other)
     {
@@ -19,8 +19,9 @@ public class rotateColumns : MonoBehaviour
             transform.Rotate(Xrotation * Time.deltaTime, Yrotation * Time.deltaTime, Zrotation * Time.deltaTime);
             for(int i = 0; i <= columnToRotate.Length; i++) { 
             columnToRotate[i].transform.Rotate(Xrotation * Time.deltaTime, -Yrotation * Time.deltaTime, Zrotation * Time.deltaTime);
+            columnToRotateInvert.transform.Rotate(Xrotation * Time.deltaTime, Yrotation * Time.deltaTime, -Zrotation * Time.deltaTime);
             }
-
+            
         }
     }
 }
