329db7254f9cd5aeb6e33fc2ff873e27ace2825c
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-01-08 12:45:28 +0100

    new platform created hexagon prefab

--- Assets/Scripts/resizeObject.cs
+++ Assets/Scripts/resizeObject.cs
@@ -8,11 +8,16 @@ public class resizeObject : MonoBehaviour {
     public int Ysize;
     public int Zsize;
 
+    public int maxSize;
+    float minSize;
+    
+
     bool resize;
 
     void Start()
     {
         resize = true;
+        minSize = transform.localScale.x;
     }
 
     void OnTriggerStay(Collider other)
@@ -20,14 +25,14 @@ public class resizeObject : MonoBehaviour {
         if (other.tag == "interactArea" && Input.GetMouseButton(0) && resize)
         {
                 transform.localScale += new Vector3(Xsize * Time.deltaTime, Ysize * Time.deltaTime, Zsize * Time.deltaTime);
-                if(transform.localScale.x > 8) {
+                if(transform.localScale.x > maxSize) {
                     resize = false;
             }
         }
         else if (other.tag == "interactArea" && Input.GetMouseButton(0) && !resize)
         {
             transform.localScale += new Vector3(-Xsize * Time.deltaTime, -Ysize * Time.deltaTime, -Zsize * Time.deltaTime);
-            if (transform.localScale.x < 3)
+            if (transform.localScale.x < minSize)
             {
                 resize = true;
             }
