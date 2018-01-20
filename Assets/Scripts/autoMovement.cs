30c73e5fef464293a7e4bbe0ba70f940e191bf7f
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-01-20 16:33:02 +0100

    -last puzzle continued -new scripts created for last puzzle -new 3d object created: door for first puzzle -new 3d object created: fan

---
+++ Assets/Scripts/autoMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoMovement : MonoBehaviour {
+
+    public GameObject target;
+
+    public bool move;
+    public bool rotate;
+    public bool scale;
+
+    public int xMove;
+    public int yMove;
+    public int zMove;
+    public int xRotate;
+    public int yRotate;
+    public int zRotate;
+    public int xScale;
+    public int yScale;
+    public int zScale;
+
+    /*
+    public int xMoveMax;
+    public int xMoveMin;
+
+    public int xRotateMax;
+    public int xRotateMin;
+
+    public int xScaleMax;
+    public int xScaleMin;
+    */
+
+
+    void Update () {
+        if (move)
+        {
+            target.transform.position.Set(xMove * Time.deltaTime, yMove * Time.deltaTime, zMove * Time.deltaTime);
+        }
+
+        if (rotate)
+        {
+            target.transform.Rotate(xRotate * Time.deltaTime, yRotate * Time.deltaTime, zRotate * Time.deltaTime);
+        }
+
+        if (scale)
+        {
+            target.transform.localScale.Set(xScale * Time.deltaTime, yScale * Time.deltaTime, zScale * Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/Scripts/playAnimsOnClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAnimsOnClick : MonoBehaviour {
+
+    public GameObject[] objToAnim;
+    
+
+    bool notAnim;
+
+
+
+    void Awake()
+    {
+        notAnim = false;
+    }
+	
+	void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "interactArea" && Input.GetMouseButton(0) && !notAnim){
+            for(int i = 0; i <= objToAnim.Length; i++) { 
+            objToAnim[i].GetComponent<Animation>().Play();
+            notAnim = true;
+            Debug.Log("Playing");
+            }
+        }
+    }
+}
