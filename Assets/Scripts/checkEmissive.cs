4b40edee84e2e926050734dbc5cb510679c02b39
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-01-23 14:03:52 +0100

    -Alternative longer wall for nextdoor created -Doors programmed

--- Assets/Scripts/checkEmissive.cs
+++ Assets/Scripts/checkEmissive.cs
@@ -9,19 +9,31 @@ public class checkEmissive : MonoBehaviour {
     public GameObject C;
     public GameObject act;
     public GameObject deact;
+    public GameObject actDoor;
+    public GameObject deactDoor;
     bool checkA;
     bool checkB;
     bool checkC;
+    bool isPlaying;
+
+    void Start()
+    {
+        isPlaying = false;
+    }
 
     void Update () {
 
         checkA = A.GetComponent<changeEmissive>().changedEmissive;
         checkB = B.GetComponent<changeEmissive>().changedEmissive;
         checkC = C.GetComponent<changeEmissive>().changedEmissive;
-        if (checkA && checkB && checkC)
+        if (checkA && checkB && checkC && !isPlaying)
         {
             act.SetActive(true);
             deact.SetActive(false);
+            actDoor.SetActive(true);
+            deactDoor.SetActive(false);
+            actDoor.GetComponent<Animation>().Play();
+            isPlaying = true;
         }
 	}
 }
