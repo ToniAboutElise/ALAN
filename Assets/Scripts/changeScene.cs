34441a6e0cd2526845cc8a02478e26718a6c1cd0
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-02-03 02:11:04 +0100

    created menu

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class changeScene : MonoBehaviour {
+
+    public string scene;
+
+    public void loadScene()
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+}
