7398a4a8fe5917ba208e6c9c313272da88533cec
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-02-20 11:32:08 +0100

    fourth puzzle designed, need to model skates

---
+++ Assets/Scripts/eyeRotateObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eyeRotateObject : MonoBehaviour {
+
+    public int XRot;
+    public int YRot;
+    public int ZRot;
+
+    public float XobjRot;
+    public float YobjRot;
+    public float ZobjRot;
+
+    public GameObject UI;
+
+    public GameObject objRotate;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "interactArea")
+        {
+            UI.SetActive(true);
+        }
+
+        if (other.name == "collisionDoor")
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            other.GetComponent<BoxCollider>().enabled = false;
+        }
+
+        if (other.tag == "interactArea" && Input.GetMouseButton(0))
+        {
+            transform.Rotate(XRot * Time.deltaTime, YRot * Time.deltaTime, ZRot * Time.deltaTime);
+
+            objRotate.transform.Rotate(XobjRot * Time.deltaTime, YobjRot * Time.deltaTime, ZobjRot * Time.deltaTime);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "interactArea")
+        {
+            UI.SetActive(false);
+        }
+    }
+}
