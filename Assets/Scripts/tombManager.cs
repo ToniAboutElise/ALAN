5a88e73a8b9d11aa7ec87006efaafbde0fcfdd37
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-04-20 00:23:43 +0200

    doors introduced

--- Assets/Scripts/tombManager.cs
+++ Assets/Scripts/tombManager.cs
@@ -7,15 +7,25 @@ public class tombManager : MonoBehaviour {
     public GameObject[] tomb;
     public bool tombsDone;
     int tombCount;
+    public GameObject doorToOpen;
+    bool opened;
 
     void Start()
     {
+        opened = false;
         tombsDone = false;
         tombCount = 0;
     }
 
     void Update()
     {
+
+        if(tombsDone == true && opened == false)
+        {
+            opened = true;
+            doorToOpen.GetComponent<Animation>().Play();
+        }
+
         for(int i = 0; i < tomb.Length; i++)
         {
             if(tomb[i].GetComponent<candleManager>().candlesDone == false)
