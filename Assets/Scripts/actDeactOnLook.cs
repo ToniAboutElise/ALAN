edf43bbcb858014848a07dfccbc5b33ea4ebfabd
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-04-20 13:07:34 +0200

    playing with people's mind starts... NOW!!

---
+++ Assets/Scripts/actDeactOnLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actDeactOnLook : MonoBehaviour {
+
+    public GameObject objToAct;
+    public GameObject objToDeact;
+    bool done;
+
+    void Start()
+    {
+        done = false;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "interactArea" && done == false)
+        {
+            objToDeact.SetActive(false);
+            objToAct.SetActive(true);
+            done = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/animateOnTriggerEnter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateOnTriggerEnter : MonoBehaviour {
+
+    public GameObject objToAnimate;
+    public GameObject objToAct;
+    public GameObject objToDeact;
+    bool hasEntered;
+    public bool afterTrigger;
+    public string animToPlay;
+
+    void Start()
+    {
+        hasEntered = false;
+    }
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && hasEntered == false)
+        {
+            hasEntered = true;
+            objToAnimate.GetComponent<Animation>().Play(animToPlay);
+            if (afterTrigger)
+            {
+                    objToAct.SetActive(true);
+                    objToDeact.SetActive(false);
+            }
+        }
+    }
+}
