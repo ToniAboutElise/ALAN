7edb2fffc033ada702aed3047019077954fcb3f2
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-08-16 12:43:32 +0200

    bridge puzzle creation has started

---
+++ Assets/Scripts/bridgeAppear.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class bridgeAppear : MonoBehaviour {
+
+    public Image[] bridgeParts;
+    public float velocity;
+    public float Fillvelocity;
+    int bridgeInt;
+    public bool test;
+
+	void Start () {
+		for(int i = 0; i <= bridgeParts.Length -1; i++)
+        {
+            bridgeParts[i].fillAmount = 0;
+        }
+        bridgeInt = 0;
+    }
+	
+    void Update()
+    {
+        if(test == true)
+        {
+            StartCoroutine(bridge());
+            test = false;
+        }
+    }
+
+    IEnumerator bridge() {
+        if(bridgeParts[bridgeInt].fillAmount != 1) { 
+            bridgeParts[bridgeInt].fillAmount += velocity;
+            yield return new WaitForSeconds(Fillvelocity);
+            StartCoroutine(bridge());
+        }
+        else if (bridgeInt != bridgeParts.Length)
+        {
+            bridgeInt += 1;
+            yield return new WaitForSeconds(Fillvelocity);
+            StartCoroutine(bridge());
+        }
+    }
+}
+
