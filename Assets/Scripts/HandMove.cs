c1e7315632b5bf50d5196cec4404faf4bd176c04
TONI\Antoni <antoni.dominguez@balmes.epiaedu.cat>
2018-08-18 18:03:06 +0200

    Ghost prototype started

---
+++ Assets/Scripts/HandMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HandMove : MonoBehaviour
+{
+    Ray nmyRay;
+    public Transform Alan;
+    bool killHim;
+    RaycastHit rayHit;
+    public float distance;
+    float distanceBodies;
+
+    void Start()
+    {
+       
+        killHim = false;
+
+    }
+
+    void FixedUpdate()
+    {
+        
+        distanceBodies = Vector3.Distance(transform.position, Alan.transform.position);
+        Debug.Log(distanceBodies);
+        //Debug.Log("one");
+        if (distanceBodies < distance)
+        {
+            //Debug.Log("player detected");
+            killHim = true;
+
+        }
+
+        if (killHim == true)
+        {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = Alan.transform.position;
+        }
+
+    }
+}
