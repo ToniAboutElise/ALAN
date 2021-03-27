8bba0dff14931304b1155254dc8efb25f2974f90
darktoni94 <tonidominguezreig@gmail.com>
2021-03-27 17:15:57 +0100

    Creates intro text scene with all necessary assets

---
+++ Assets/Scripts/LanguageAsset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageAsset : MonoBehaviour {
+
+    public Text text;
+
+    public Language language;
+
+    public enum Language
+    {
+        English,
+        Spanish
+    }
+
+    public TextLanguage textLanguage;
+
+    [System.Serializable]
+    public struct TextLanguage
+    {
+        public string English;
+        public string Spanish;
+    }
+
+    private void Start()
+    {
+        text.color = new Color(1, 1, 1, 0);
+    }
+
+}
---
+++ Assets/Scripts/TransitonTextSequence.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitonTextSequence : MonoBehaviour {
+
+    public string nextScene;
+
+	public List <LanguageAsset> languageAssetList;
+
+    protected bool canPressContinue = false;
+
+    private void Start()
+    {
+        StartCoroutine(TextAppear());
+    }
+
+    protected IEnumerator TextAppear()
+    {
+        yield return new WaitForSeconds(2);
+        foreach(LanguageAsset la in languageAssetList)
+        {
+            la.GetComponent<Animation>().Play("textAppearTransitionScene");
+
+            if(la != languageAssetList[languageAssetList.Count-1])
+            yield return new WaitForSeconds(9);
+        }
+        canPressContinue = true;
+    }
+
+    protected IEnumerator TextDisappear()
+    {
+        yield return new WaitForSeconds(2);
+        foreach (LanguageAsset la in languageAssetList)
+        {
+            la.GetComponent<Animation>().Play("textDisappearTransitionScene");
+        }
+        yield return new WaitForSeconds(2.4f);
+        SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Single);
+    }
+
+    private void Update()
+    {
+        if(canPressContinue == true && (Input.GetMouseButton(0) || Input.GetButtonDown("Fire1")))
+        {
+            canPressContinue = false;
+            StartCoroutine(TextDisappear());
+        }
+    }
+
+}
