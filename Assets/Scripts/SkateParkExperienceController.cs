25de2c87a7b1b2e28a80014da8511da0ce139a9f
darktoni94 <tonidominguezreig@gmail.com>
2021-03-29 19:14:01 +0200

    Fixes bug for next level trigger

--- Assets/Scripts/SkateParkExperienceController.cs
+++ Assets/Scripts/SkateParkExperienceController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SkateParkExperienceController : MonoBehaviour {
 
+    private int exercisesCompleted = 0;
     public List<SkateParkExercise> exerciseList;
 
     public void CheckCompletedExercises()
     {
+        exercisesCompleted++;
+        if (exercisesCompleted == exerciseList.Count)
         FindObjectOfType<LevelManager>().NextLevel("");
     }
 }
