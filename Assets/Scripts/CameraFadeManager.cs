59e5d26c2ce3b5edfcbd8e97a777f462cb73ee8f
darktoni94 <tonidominguezreig@gmail.com>
2021-03-29 18:54:47 +0200

    Modifies light and concrete material and implements fade

---
+++ Assets/Scripts/CameraFadeManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class CameraFadeManager : MonoBehaviour {
+
+    public Animation fadeAnim;
+
+    public void FadeOut()
+    {
+        fadeAnim.Play("sphereFadeOut");
+    }
+
+    public void FadeIn()
+    {
+        fadeAnim.Play("sphereFadeIn");
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    protected CameraFadeManager fadeManager;
+
+    private void Start()
+    {
+        fadeManager = FindObjectOfType<CameraFadeManager>();
+        fadeManager.FadeOut();
+    }
+}
