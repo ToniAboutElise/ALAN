06456fec24614b5d904e35d7cc25b38e53437212
darktoni94 <tonidominguezreig@gmail.com>
2021-03-30 00:36:08 +0200

    Creates transition to limbo level and prefab to make story text appear on trigger enter

---
+++ Assets/Scripts/StoryTextTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTextTrigger : MonoBehaviour {
+
+    public StoryText storyText;
+    protected bool played = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(played == false)
+        {
+            played = true;
+            storyText.PlayTextSquence();
+        }
+    }
+}
